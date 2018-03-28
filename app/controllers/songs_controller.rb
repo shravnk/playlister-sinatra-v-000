0115a1042358e2fd7f173c7434e55899d498ef99
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-28 05:31:49 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -35,6 +35,7 @@ class SongsController < ApplicationController
   end
 
   patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
     @artist = Artist.find_or_create_by(name: params['artist_name'])
     @song.genre_ids = params["genres"]
     @song.artist = @artist
